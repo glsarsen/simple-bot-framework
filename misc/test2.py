64bb8961fd947f28e463bd3c7bd0c6a42ced2af2
Arsenii Hlazov <glazov.arseniy@gmail.com>
2023-04-10 10:38:40 +0300

    deleted unnecessary files, created run script

--- misc/test2.py
+++
@@ -1,9 +0,0 @@
-from itertools import islice
-
-def chunks(data, SIZE=10000):
-    it = iter(data)
-    for i in range(0, len(data), SIZE):
-        yield {k:data[k] for k in islice(it, SIZE)}
-
-for item in chunks({k:k for k in range(4)}, 5):
-    print(item)
---
+++ run_bot.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+source venv/bin/activate
+gunicorn -w 2 wsgi:app --log-file bot.log -b 127.0.0.1:30000
+
+sleep 10s
+python3.9 viber_webhook.py
